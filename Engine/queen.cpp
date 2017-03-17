bf43e156bfb93b7666bef4c67b0587b7b92dc687
Nick Keithley <nicholas.keithley@live.longwood.edu>
2017-03-17 17:36:44 -0400

    Implemented isValid for queen.cpp

--- Engine/queen.cpp
+++ Engine/queen.cpp
@@ -1,31 +1,81 @@
 #include "queen.h"
 using namespace std;
 
-bool Queen::isValid()
+vector<coords> Queen::isValid()
 {
-  //Used to be param, need to change this
-  string current = getPosition();
-  //Checking color first.
-  if (getColor=='w')
-  {
-	
-  }
+	coords temp;
+	vector<coords> set;
 
-  else if (getColor == 'b')
-  {
-	
-  }
+	//Think we don't need to check color.
+	//Queen can move the same no matter what color.
+
+	//Moving vertically towards "top" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y + 1;
+		temp.x = m_position.x + 0;
+		set.push_back(temp)
+	}
+
+	//Moving vertically towards "bottom" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y - 1;
+		temp.x = m_position.x + 0;
+		set.push_back(temp)
+	}
+
+	//Moving horizontally towards "right" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y + 0;
+		temp.x = m_position.x + 1;
+		set.push_back(temp)
+	}
+
+	//Moving horizontally towards "left" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y + 0;
+		temp.x = m_position.x - 1;
+		set.push_back(temp)
+	}
+
+	//Moving diagonally towards "top right" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y + 1;
+		temp.x = m_position.x + 0;
+		set.push_back(temp)
+	}
+
+	//Moving diagonally towards "top left" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y + 1;
+		temp.x = m_position.x - 1;
+		set.push_back(temp)
+	}
+
+	//Moving diagonally towards "bottom right" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y - 1;
+		temp.x = m_position.x + 1;
+		set.push_back(temp)
+	}
+
+	//Moving diagonally towards "bottom left" of board:
+	for (int i = 0; i < 8; ++i)
+	{
+		temp.y = m_position.y - 1;
+		temp.x = m_position.x - 1;
+		set.push_back(temp)
+	}
+
 }
-
 void Queen::Move(const string posiiton)
 {
   //Check if valid move, then: 
   setPosition(position);
 }
-/*
-void Queen::Attack(const string position, Piece* piece)
-{
-  piece->setCaptured(true);
-  setPosition(position);
-}
-*/
