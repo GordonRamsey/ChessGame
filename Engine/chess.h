fc67566febf2a26aea56498b368b64374e6e9102
David Lackey <david.lackey@live.longwood.edu>
2017-03-17 17:02:44 -0400

    Started working on ValidMoves() in chess.cpp, added a chess.h file to clean up the header section, and so it can be included later in other files

--- Engine/chess.h
+++ Engine/chess.h
@@ -1,5 +1,16 @@
 #ifndef CHESS_H
 #define CHESS_H
+#include <vector>
+#include "piece.h"
+#include "pawn.h"
+#include "rook.h"
+#include "knight.h"
+#include "bishop.h"
+#include "queen.h"
+#include "king.h"
+using namespace std;
 
-/*
-    
+void SetUp();
+void Move(int cur_x, int cur_y, int new_x, int new_y);
+vector<coord> ValidMoves(const int x, const int y);
+   
