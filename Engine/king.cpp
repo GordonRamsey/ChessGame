da61ccfb8b0ea41376649013ef2a51ef389fcfe2
Mija Isip <mija.isip@live.longwood.edu>
2017-03-21 13:23:21 -0400

    fixing logic errors in isValid()

--- Engine/king.cpp
+++ Engine/king.cpp
@@ -6,23 +6,24 @@ vector<coords> King::isValid()
     coords temp; 
     vector<coords> set; 
     //move forward (w) back (b)  
-        temp.y = m_position.y + 1; 
-        temp.x = m_position.x + 0; 
+    temp.y = m_position.y + 1; 
+    temp.x = m_position.x + 0; 
+    if((temp.y && temp.x) < 8 && (temp.y && temp.x) >= 0)
         set.push_back(temp);
-    
     //move back(w) forward (b)
-        temp.y = m_position.y - 1; 
-        temp.x = m_position.x + 0; 
-        set.push_back(temp);    
-    
+    temp.y = m_position.y - 1; 
+    temp.x = m_position.x + 0; 
+    if((temp.y && temp.x) < 8 && (temp.y && temp.x) >= 0)
+            set.push_back(temp);    
     //move right (w) left (b)
-        temp.y = m_position.y + 0; 
-        temp.x = m_position.x + 1; 
+    temp.y = m_position.y + 0; 
+    temp.x = m_position.x + 1; 
+    if((temp.y && temp.x) < 8 && (temp.y && temp.x) >= 0)
         set.push_back(temp);
-    
     //move left (w) right(b) 
-        temp.y = m_position.y + 0; 
-        temp.x = m_position.x - 1; 
+    temp.y = m_position.y + 0; 
+    temp.x = m_position.x - 1; 
+    if((temp.y && temp.x) < 8 && (temp.y && temp.x) >= 0)
         set.push_back(temp);
 }
 
