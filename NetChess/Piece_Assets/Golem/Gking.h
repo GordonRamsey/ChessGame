f9d0142b787caa731612ba270bdc94d164dc0252
David Lackey <david.lackey@live.longwood.edu>
2017-04-23 17:41:12 -0400

    Changed Fking to actually work, and worked on Golems

--- NetChess/Piece_Assets/Golem/Gking.h
+++ NetChess/Piece_Assets/Golem/Gking.h
@@ -11,6 +11,7 @@ class GKing : public King
     string Move(coord newpos);
     vector<coord> validSpots(Chess* c);
     string processClicks(vector<coord> clickedOn, Chess* c) 
+    string MakeRocks(Chess* c);
     bool isRock() const;
     void Rock()
 };
