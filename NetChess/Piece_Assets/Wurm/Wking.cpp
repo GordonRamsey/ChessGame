6c2546d1e57db06dade3604f033eff676e3bf888
Nick Keithley <nicholas.keithley@live.longwood.edu>
2017-04-30 15:10:19 -0400

    Started messing with Wurm class more a while ago

--- NetChess/Piece_Assets/Wurm/Wking.cpp
+++ NetChess/Piece_Assets/Wurm/Wking.cpp
@@ -1,1 +1,21 @@
-//Wurm King
+#include "Wking.h"
+
+WKing::WKing(int x, int y, int it) : King(x, y, it)
+{
+	debug_name = "WKing";
+}
+
+WKing::~WKing()
+{}
+
+string WKing::Move(coord newpos)
+{
+	stringstream ss;
+	ss << "MOVE " << getNum() << " " << newpos.x*64 << newpos.y*64 << " ~";
+	return ss.str();
+}
+
+vector<coord> WKing::validSpots(Chess* c)
+{
+	return King::validSpots(c); //according to whoever wrote Fking, this returns normal king movements.
+}
