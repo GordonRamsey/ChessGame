acde0a5befb80fb2f5465a281161208398a94942
HanYolo <thomas.morgan@live.longwood.edu>
2017-05-01 21:15:57 -0400

    will compile now.

--- NetChess/Piece_Assets/Necro/Nking.h
+++ NetChess/Piece_Assets/Necro/Nking.h
@@ -8,8 +8,8 @@ class NKing : public King
     NKing(int x, int y, int it);
     ~NKing();
     string Move(coord newpos);
-    vector<coord> validSLoc(Chess* c); 
-    bool isValid(vector<coord> validSLoc, vector<coord> clickedOn, Chess* c); 
+//    vector<coord> validSLoc(Chess* c); 
+//    bool isValid(vector<coord> validSLoc, vector<coord> clickedOn, Chess* c); 
     vector<coord> validSpots(Chess* c);
     string processClicks(vector<coord> clickedOn, Chess* c); 
     vector<coord> validSLoc(Chess* c);
